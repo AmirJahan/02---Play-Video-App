1f4e9f4b7b4234accbfd8894607a03452e7e0537
Amir J <amir.jahan@gmail.com>
2017-10-19 10:24:08 -0700

    Initial Commit

---
+++ Play Video App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Play Video App
+//
+//  Created by Amir on 10/19/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
